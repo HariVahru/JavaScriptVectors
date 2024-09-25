9ffb4633d3e088dd7210a1953c3f92b2ceaedca7
Harry <igor090763@gmail.com>
2024-09-25 21:04:42 +0300

    Added a new component that draws an the line by taking all the information about the vector from vector class

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 import Vector2 from "../src/classes/vector2"
+import VectorDrawing from './classes/VectorDrawing';
 
 function App() {
 
+  const [vectorData, setVectorData] = useState({
+    x: null,
+    y: null,
+    rad: null,
+    theta: null,
+    value: null,
+  });
+
+
   const runTest = () =>
   {
-    let v = new Vector2(20,405);
+    let v = new Vector2(50,105);
     console.log(v.getValue())
     console.log(v.getTheta())
     console.log(v.getRad())
     console.log(v.getX())
     console.log(v.getY())
+    setVectorData({
+      x: v.getX(),
+      y: v.getY(),
+      rad: v.getRad(),
+      theta: v.getTheta(),
+      value: v.getValue(),
+    });
   }
 
   return (
     <div className="App">
+      <VectorDrawing value={vectorData.value} theta={vectorData.theta} rad={vectorData.rad} x={vectorData.x} y={vectorData.y}/>
       <button onClick={() => runTest()} >Run test</button>
     </div>
   );
