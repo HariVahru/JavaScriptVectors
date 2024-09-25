fe838cf4c2e4dca6946213c2170a2fd671168e39
DESKTOP-RNHNU2V\lolki <Obragius>
2024-09-25 23:19:16 +0100

    Created a point class to store points for shapes. This will represent the origin of the shape

---
+++ src/classes/point.js
@@ -0,0 +1,42 @@
+class Point
+{
+    #x;
+    #y;
+
+    constructor(x,y)
+    {
+        this.#x = x;
+        this.#y = y;
+    }
+
+    setX(value)
+    {
+        this.#x = value
+    }
+
+    setY(value)
+    {
+        this.#y = value
+    }
+
+    setPos(x,y)
+    {
+        this.#x = x;
+        this.#y = y;
+    }
+
+    getPos()
+    {
+        return [this.#x,this.#y]
+    }
+
+    getX()
+    {
+        return this.#x
+    }
+
+    getY()
+    {
+        return this.#y
+    }
+}
